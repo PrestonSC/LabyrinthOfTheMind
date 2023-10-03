2029ca05d5f47a43e904b6dae99c40f1b7f76854
PrestonSC <prestoncunningham01@gmail.com>
2023-10-03 14:44:13 -0400

    Added exit game button

--- LabyrinthOfTheMind/LabyrinthOfTheMind/MainMenuState.cpp
+++ LabyrinthOfTheMind/LabyrinthOfTheMind/MainMenuState.cpp
@@ -30,16 +30,24 @@ void MainMenuState::initKeybinds()
 	ifs.close();
 }
 
+void MainMenuState::initButtons()
+{
+	this->buttons["GAME_STATE"] = new Button(100, 100, 150, 50,
+		&this->font, "New Game",
+		sf::Color(70, 70, 70, 200), sf::Color(150, 150, 150, 255), sf::Color(20, 20, 20, 200));
+
+	this->buttons["QUIT_STATE"] = new Button(100, 300, 150, 50,
+		&this->font, "Quit",
+		sf::Color(100, 100, 100, 200), sf::Color(150, 150, 150, 255), sf::Color(20, 20, 20, 200));
+}
+
 //Constructors/Destructors
 MainMenuState::MainMenuState(sf::RenderWindow* window, std::map<std::string, int>* supportedKeys)
 	: State(window, supportedKeys)
 {
 	this->initFonts();
 	this->initKeybinds();
-
-	this->gameState_btn = new Button(100, 100, 150, 50,
-		&this->font, "New Game", 
-		sf::Color(70, 70, 70, 200), sf::Color(150, 150, 150, 255), sf::Color(20, 20, 20, 200));
+	this->initButtons();
 
 	this->background.setSize(sf::Vector2f(window->getSize().x, window->getSize().y));
 	this->background.setFillColor(sf::Color::Magenta);
@@ -47,7 +55,11 @@ MainMenuState::MainMenuState(sf::RenderWindow* window, std::map<std::string, int
 
 MainMenuState::~MainMenuState()
 {
-	delete this->gameState_btn;
+	auto it = this->buttons.begin();
+	for (it = this->buttons.begin(); it != this->buttons.end(); ++it)
+	{
+		delete it->second;
+	}
 }
 
 
@@ -61,12 +73,41 @@ void MainMenuState::updateInput(const float& dt)
 	this->checkForQuit();
 }
 
+void MainMenuState::updateButtons()
+{
+	//Updates all the buttons in the state
+	for (auto& it : this->buttons)
+	{
+		it.second->update(this->mousePosView);
+	}
+
+	//Check for new game
+	if (this->buttons["GAME_STATE"]->isPressed())
+	{
+		//this->states.push(new MainMenuState(this->window, &this->supportedKeys));
+	}
+
+	//Check for quit
+	if (this->buttons["QUIT_STATE"]->isPressed())
+	{
+		this->quit = true;
+	}
+}
+
 void MainMenuState::update(const float& dt)
 {
 	this->updateMousePositions();
 	this->updateInput(dt);
 
-	this->gameState_btn->update(this->mousePosView);
+	this->updateButtons();
+}
+
+void MainMenuState::renderButtons(sf::RenderTarget* target)
+{
+	for (auto& it : this->buttons)
+	{
+		it.second->render(target);
+	}
 }
 
 void MainMenuState::render(sf::RenderTarget* target)
@@ -76,5 +117,5 @@ void MainMenuState::render(sf::RenderTarget* target)
 
 	target->draw(this->background);
 
-	this->gameState_btn->render(target);
+	this->renderButtons(target);
 }
