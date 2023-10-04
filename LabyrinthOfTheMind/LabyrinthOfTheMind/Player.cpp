b1f1b2dc4b45c21a2c9ff3f8821638de44262ae2
PrestonSC <prestoncunningham01@gmail.com>
2023-10-04 17:07:16 -0400

    Added initial player animation

--- LabyrinthOfTheMind/LabyrinthOfTheMind/Player.cpp
+++ LabyrinthOfTheMind/LabyrinthOfTheMind/Player.cpp
@@ -7,19 +7,30 @@ void Player::initVariables()
 
 void Player::initComponents()
 {
-	this->createMovementComponent(300.f, 15.f, 5.f);
+	
 }
 
 //Constructors/Destructors
-Player::Player(float x, float y, sf::Texture& texture)
+Player::Player(float x, float y, sf::Texture& texture_sheet)
 {
 	this->initVariables();
-	this->initComponents();
 
-	this->setTexture(texture);
 	this->setPosition(x, y);
+
+	this->createMovementComponent(300.f, 15.f, 5.f);
+	this->createAnimationComponent(texture_sheet);
+
+	this->animationComponent->addAnimation("RUN_RIGHT", 10.f, 0, 0, 5, 0, 200, 148);
 }
 
 Player::~Player()
 {
+}
+
+//Functions
+void Player::update(const float& dt)
+{
+	this->movementComponent->update(dt);
+
+	this->animationComponent->play("RUN_RIGHT", dt);
 }
