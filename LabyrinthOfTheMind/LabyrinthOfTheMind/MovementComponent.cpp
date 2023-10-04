f2756b47440e8bdf833284163aea1b5ee04f9de3
PrestonSC <prestoncunningham01@gmail.com>
2023-10-04 13:10:53 -0400

    Acceleration deceleration added

--- LabyrinthOfTheMind/LabyrinthOfTheMind/MovementComponent.cpp
+++ LabyrinthOfTheMind/LabyrinthOfTheMind/MovementComponent.cpp
@@ -1,7 +1,8 @@
 #include "MovementComponent.h"
 
-MovementComponent::MovementComponent(sf::Sprite& sprite, float maxVelocity)
-	: sprite(sprite), maxVelocity(maxVelocity)
+MovementComponent::MovementComponent(sf::Sprite& sprite, float maxVelocity,
+	float acceleration, float deceleration)
+	: sprite(sprite), maxVelocity(maxVelocity), acceleration(acceleration), deceleration(deceleration)
 {
 	this->maxVelocity = maxVelocity;
 }
@@ -21,13 +22,42 @@ const sf::Vector2f& MovementComponent::getVelocity() const
 //Functions
 void MovementComponent::move(const float dir_x, const float dir_y, const float dt)
 {
-	this->velocity.x = this->maxVelocity * dir_x;
-	this->velocity.y = this->maxVelocity * dir_y;
+	/* Accelerating a sprite until it reaches the max velocity */
 
-	this->sprite.move(this->velocity * dt); // Uses velocity
+	//Acceleration
+	this->velocity.x += this->acceleration * dir_x;
+
+	if (this->velocity.x > 0.f) //Check for right movement
+	{
+		if (this->velocity.x > this->maxVelocity)
+			this->velocity.x = this->maxVelocity;
+	}
+	else if (this->velocity.x < 0.f) // Check for left movement
+	{
+		if (this->velocity.x < -this->maxVelocity)
+			this->velocity.x = -this->maxVelocity;
+	}
+	
+
+	this->velocity.y += this->acceleration * dir_y;
 }
 
 void MovementComponent::update(const float& dt)
 {
+	//Deceleration
+	if (this->velocity.x > 0.f) //Check for right movement
+	{
+		this->velocity.x -= deceleration;
+		if (this->velocity.x < 0.f)
+			this->velocity.x = 0.f;
+	}
+	else if (this->velocity.x < 0.f)
+	{
+		this->velocity.x += deceleration;
+		if (this->velocity.x > 0.f)
+			this->velocity.x = 0.f;
+	}
 
+	//Final move
+	this->sprite.move(this->velocity * dt); // Uses velocity
 }
